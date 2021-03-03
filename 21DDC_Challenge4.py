1ed0c0f880b096faf34b988b74575868009e43b4
William Lee <77645617+williamjaeholee@users.noreply.github.com>
2021-03-02 20:58:05 -0800

    Update 21DDC_Challenge4.py

--- 21DDC_Challenge4.py
+++ 21DDC_Challenge4.py
@@ -12,13 +12,13 @@ Dot needs to purchase:
 4. 165 liters of Paint Finish
 
  -------------------------------------------------------------------------------------------------
-|     Item	                Needed Amount to Buy	        Wholesale Price	         Retail Price |
- -------------------------------------------------------------------------------------------------     
-| Plank of Oak Wood	               600	                        $ 7000	               $ 12.99    |
-| 1 Liter of Blue Paint	           150	                        $ 1000	               $ 8.99     |
-| 1 Liter of White Paint	       15	                        $ 1000	               $ 9.99     |
-| 1 Liter of Paint Finish	       165	                        $ 800	               $ 3.99     |
---------------------------------------------------------------------------------------------------
+|     Item	                Needed Amount to Buy	        Wholesale Price	         Retail Price     |
+ ------------------------------------------------------------------------------------------------- 
+| Plank of Oak Wood	               600	                      $ 7000	               $ 12.99        |
+| 1 Liter of Blue Paint	           150	                      $ 1000	               $ 8.99         |
+| 1 Liter of White Paint	          15	                       $ 1000	               $ 9.99         |
+| 1 Liter of Paint Finish	         165	                      $ 800	                $ 3.99         |
+ -------------------------------------------------------------------------------------------------
 
 Use a loop to determine the price Dot would pay for purchasing supplies at the retail price. Based on that calculation, which itmes should Dot buy at retail vs. wholesale?
 
