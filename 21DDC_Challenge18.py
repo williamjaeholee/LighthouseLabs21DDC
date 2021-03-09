cabc4f0e6a0343b9f50453dec9baf606aa23972c
William Lee <77645617+williamjaeholee@users.noreply.github.com>
2021-03-09 13:59:06 -0800

    Update 21DDC_Challenge18.py

--- 21DDC_Challenge18.py
+++ 21DDC_Challenge18.py
@@ -42,6 +42,21 @@ Card Game                24
 Economic, Trains         14
 Fantasy                  14
 Name: category, dtype: int64
+        
+# OR: with bar plots
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df.category.value_counts().head(10).index
+
+plt.figure(figsize = (14,7))
+plt.bar(df.category.value_counts().head().index ,height = df.category.value_counts().head(), color = 'red')
+plt.title('This is a bar plot!', fontsize =14) #Specifying a title
+plt.xlabel('This is the x axis!', fontsize = 14)
+plt.xticks(rotation = 'vertical')
+plt.ylabel('This it the y axis!', fontsize = 14)
+plt.show()
 
 # 2. Which categories of boardgames that are not targeted for young children are the same compared to the top 5 boardgames categories in the overall dataset?
 
@@ -64,3 +79,16 @@ np.intersect1d(categoryFilter, dataFrame)
 
 array(['Card Game', 'Card Game, Fantasy', 'Wargame, World War II'],
       dtype=object)
+
+# OR: with bar plots
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+age_filter = df.age >= 13
+df_2 = df[age_filter]
+
+plt.bar(df_2.category.value_counts().head(5).index ,height = df_2.category.value_counts().head(5), color = 'red')
+plt.title('Top 5, for above 13!', fontsize =14) #Specifying a title
+plt.xticks(rotation = 'vertical')
+plt.show()
